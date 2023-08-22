7f3401028771740e141f392cfe76a7443ec258ff
Nanna Ellegaard <nel@adm.ku.dk>
2023-08-22 09:50:29 +0200

    Init

---
+++ Configuration/Icons.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Icon Registry
+ */
+
+defined('TYPO3') || die();
+
+return [
+    // icon identifier
+    'ucph_ce_csv_icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:ucph_ce_csv/Resources/Public/Icons/csv.svg',
+    ],
+];
---
+++ README.md
@@ -0,0 +1,6 @@
+TYPO3 extension content element "CSV"
+==============================================================
+
+Render a .csv file as a table.
+
+TYPO3 v. 11.5, PHP 8.1.
